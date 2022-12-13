31a8783f9448bffc33e25cb8d986adbb6ddfd180
bladeofcadence <bladeofcadence@gmail.com>
2022-12-12 19:45:22 -0600

    Initial Commit

---
+++ KCalendar/KCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  KCalendarApp.swift
+//  KCalendar
+//
+//  Created by Kelli Norris on 12/12/22.
+//
+
+import SwiftUI
+
+@main
+struct KCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
